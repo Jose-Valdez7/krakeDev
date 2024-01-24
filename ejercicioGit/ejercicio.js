a9390b9ba284d4b28635cf1dc8463e9a55e6917f
Jose-Valdez7 <joxzsian@gmail.com>
2024-01-23 23:23:41 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpImagenSaludo
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./Imagenes/Gif2.gif"
     console.log("ingresar a saludar")
+
 }
