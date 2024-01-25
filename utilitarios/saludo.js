88b47eca28f59023893c887aeb21a9684fd219ee
Jose-Valdez7 <joxzsian@gmail.com>
2024-01-24 21:13:07 -0500

    se agregaron las funciones de entero y float ok

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -4,6 +4,10 @@ saludar=function(){
     // recuperar el valor de la caja de texto txtApellido
     let apellido=recuperarTexto("txtApellido");
 
+    let edad=recuperarInt("txtEdad");
+    
+    let estatura=recuperarFloat("txtEstatura");
+
 }
 recuperarTexto=function(idComponente){
     let componente;
@@ -11,4 +15,16 @@ recuperarTexto=function(idComponente){
     componente=document.getElementById(idComponente);
     valorIngresado=componente.value 
     return valorIngresado
+}
+
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero
+}
+
+recuperarFloat=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorFlotante=parseFloat(valorCaja);
+    return valorFlotante
 }
