b96394f474a7d6d111221a119a96c4d1636c6c8b
Jose-Valdez7 <joxzsian@gmail.com>
2024-02-15 20:14:46 -0500

    Paso 0 completado

---
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -0,0 +1,11 @@
+//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+
+esMayuscula=function(caracter){
+    let letra=caracter
+    let codigo=letra.charCodeAt(0)
+    if(codigo>=65 && codigo<=90){
+        return true;
+    }else {
+        return false;
+    }
+}
